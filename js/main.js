ff9176a05f47999cfcb56addade4a6b4aaaad644
Claudia De Leon <claulg2701@gmail.com>
2016-03-18 17:04:46 -0400

    overrite submit form action

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+function logFormToConsole ( event ) {
+    event.preventDefault();
+    console.log( 'The firstName is: ' + document.getElementById( 'firstName' ).value );
+    console.log( 'The lastName is: ' + document.getElementById( 'lastName' ).value );
+    console.log( 'The email is: ' + document.getElementById( 'email' ).value );
+    console.log( 'The message is: ' + document.getElementById( 'message' ).value );
+}
+
+var onSubmit = document.getElementById( 'submitBtn' );
+onSubmit.addEventListener( 'click', logFormToConsole, false );
